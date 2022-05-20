228f44bef03ee7ce729ef08cb76c11aa43651884
ilir pllana <ilirpllana95@gmail.com>
2022-05-19 23:32:12 -0400

    added basic code

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "tutorial-5a2cf.firebaseapp.com",
-  projectId: "tutorial-5a2cf",
-  storageBucket: "tutorial-5a2cf.appspot.com",
-  messagingSenderId: "585126334212",
-  appId: "1:585126334212:web:8539eafc56885e1b4c4a51"
+  apiKey: "AIzaSyDq6la42-IQdqiIqT4mvIktzy0OZ9eWypc",
+  authDomain: "tesla-login-764cc.firebaseapp.com",
+  projectId: "tesla-login-764cc",
+  storageBucket: "tesla-login-764cc.appspot.com",
+  messagingSenderId: "677854154634",
+  appId: "1:677854154634:web:8ef468fabe568303c88273"
 };
 
 const app = initializeApp(firebaseConfig);
